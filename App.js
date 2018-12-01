0298e3ead2be9c1641fa92842045901b23cd8ed7
Supunsashika <supunsashika12345@gmail.com>
2018-12-01 13:00:06 +0530

    update

--- App.js
+++ App.js
@@ -26,11 +26,11 @@ export default class App extends Component<Props> {
     });
   }
 
-  
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native Updated App!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         <TouchableOpacity onPress={this.onButtonPress}>
