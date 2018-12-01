ea0132c47a77573a50d197f5fb25435a8939c37c
Supunsashika <supunsashika12345@gmail.com>
2018-12-01 12:49:32 +0530

    Added button

--- App.js
+++ App.js
@@ -6,24 +6,36 @@
  * @flow 
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text,TouchableOpacity, View } from 'react-native';
+import codePush from "react-native-code-push";
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu This is IOS!!s',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'This is Android!!! Live reload' ,
+    'This is Android!!! Live reload',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  onButtonPress() {
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+  }
+
+  
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity onPress={this.onButtonPress}>
+          <Text>Check for updates</Text>
+        </TouchableOpacity>
       </View>
     );
   }
